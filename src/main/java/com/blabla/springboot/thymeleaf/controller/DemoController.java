e479fa9a2b78688f4061f88071e57051fb20624f
abdelfttah95 <abdelfttah.sameeh@gmail.com>
2022-01-14 11:52:52 +0200

    first commit

---
+++ src/main/java/com/blabla/springboot/thymeleaf/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.blabla.springboot.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author abdel
+ */
+@Controller
+public class DemoController {
+    
+    @GetMapping("/hello")
+    public String sayHello(Model theModel){
+        theModel.addAttribute("theDate", new java.util.Date());
+        
+        return "helloworld";
+    }
+}
---
+++ src/main/java/com/blabla/springboot/thymeleaf/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.blabla.springboot.thymeleaf.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.blabla.springboot.thymeleaf.entity.Employee;
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+    
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+}
